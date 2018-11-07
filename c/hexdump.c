dec23bf20de97c78ff9bd32454b6fdb329f14199
cz7asm <cz7asm@gmail.com>
2018-11-07 15:13:39 +0100

    Update hexdump.c
    fix of fix

--- c/hexdump.c
+++ c/hexdump.c
@@ -12,13 +12,13 @@ void hexdump(const void *src, size_t size)
 
     for (size_t i=0; i < size; i++) {
         printf("%02X ", src8[i]);
-        int rem = (i+1)%CNT;
-        if ((i && !rem) || i+1==size) {
-            if (rem)
-                for (int j=0; j < rem; j++)
-                    printf("   ");
+        int n = i % CNT;
+        if ((i && n==CNT-1) || i+1==size) {
+            int rem = CNT-1 - n;
+            for (int j=0; j < rem; j++)
+                printf("   ");
             printf("|");
-            for (int j=CNT-rem; j > 0; j--)
+            for (int j=n; j >= 0; j--)
                 putchar(isprint(src8[i-j])?src8[i-j]:'.');
             printf("|\n");
         }
